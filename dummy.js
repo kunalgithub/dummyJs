4b8cd5ab456cf6c7d96aa30005b3f6a3ada4373f
kunalgithub <kunal.kishan@gmail.com>
2017-09-02 12:16:19 +0530

    ok
    ok

--- dummy.js
+++ dummy.js
@@ -1,6 +1,15 @@
 function iamReady(){
-    console.log("Access to XHR",xhr);
-	$(document).foundation();
-	console.log("Foosa Hakuna Matata");
+    console.log("Entering iamReady ");
+    if(xhr.readyState===4){
+    	console.log("Response has Come - I can continue with What I want ",xhr);
+    	$(document);
+    }else{
+    	console.log("Not ReadyState 4 ~~ What should i do ?",xhr.readyState);
+    }
+	
 }
-iamReady();
+iamReady();//Execute it 
+
+function iamReady2(){
+	 console.log("Entering iamReady2~~~~",xhr.readyState);
+}//Dont execute it
