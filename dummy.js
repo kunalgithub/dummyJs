afe0188b552eca2a8c2092b005dedb02a77d565e
kunalgithub <kunalkishan@Kunals-MacBook-Pro.local>
2017-09-02 10:55:53 +0530

    changes

--- dummy.js
+++ dummy.js
@@ -1,5 +1,5 @@
 function iamReady(){
-        console.log(‘Do I have access to xhr’, xhr);
+        console.log(“Do I have access to xhr”, xhr);
 	$(document).foundation();
 	console.log('Foosa Hakuna Matata');
 }
