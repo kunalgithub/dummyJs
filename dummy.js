4dd1bc3f850dc3dcf598a24db4bbd62e04dc6c6c
kunalgithub <kunal.kishan@gmail.com>
2017-09-03 16:41:54 +0530

    more

--- dummy.js
+++ dummy.js
@@ -3,5 +3,8 @@ specialRequest(function(error, xhr) { console.log('custom-Function',error,xhr);
         console.log('error:',error);
     } else {
          console.log('xhr:',xhr);
+         if(xhr.readyState===4 && status===200){
+         	console.log('will call foundation here')
+         }
     }
 });
