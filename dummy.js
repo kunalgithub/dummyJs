9bfd7df0b223e31b7cd539fbc09cc5741024905b
kunalgithub <kunal.kishan@gmail.com>
2017-09-03 16:27:18 +0530

    changed
    changed

--- dummy.js
+++ dummy.js
@@ -1,15 +1,7 @@
-function iamReady(){
-    console.log("Entering iamReady ");
-    if(xhr.readyState===4){
-    	console.log("Response has Come - I can continue with What I want ",xhr);
-    	$(document);
-    }else{
-    	console.log("Not ReadyState 4 ~~ What should i do ?",xhr.readyState);
+specialRequest(function(error, xhr) { console.log('custom-Function',error,xhr);
+    if (error) {
+        console.log('error:',error);
+    } else {
+         console.log('xhr:',xhr);
     }
-	
-}
-iamReady();//Execute it 
-
-function iamReady2(){
-	 console.log("Entering iamReady2~~~~",xhr.readyState);
-}//Dont execute it
+});
