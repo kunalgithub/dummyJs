8ef9f94b6cee24dfea24e0a68b660107303598a1
kunalgithub <kunal.kishan@gmail.com>
2017-09-04 20:25:50 +0530

    Promise Code

--- dummy.js
+++ dummy.js
@@ -1,13 +1,5 @@
-if(xhr.readyState===4 && xhr.status===200){
-	console.log('custom-Function',xhr);
-}
-specialRequest(function(error, xhr) { console.log('custom-Function',error,xhr);
-    if (error) {
-        console.log('error:',error);
-    } else {
-         console.log('xhr:',xhr);
-         if(xhr.readyState===4 && xhr.status===200){
-         	console.log('will call foundation here')
-         }
-    }
-});
+_$rfiHubPromise.then(function(response) {
+	console.log("Success!", response);
+}, function(error) {
+	console.error("Failed!", error);
+})
